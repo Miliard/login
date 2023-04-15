e3adb625ce8419497526f55a3566732a1bb5e3e0
wil <elsalvadorazul@outlook.es>
2023-04-15 14:22:01 -0600

    voy para el cap 7

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\InicioController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,9 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'App\Http\Controllers\InicioController@index');
 
 Route::middleware([
     'auth:sanctum',
