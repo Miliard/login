46ebf36812b323592073e9e140e4c47e41aec34c
telemaco <elchwplm@gmail.com>
2023-04-14 21:29:40 -0600

    rutas listo cap 5

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,50 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+//ejmplo1
+Route::get('/texto', function () {
+    return'<h1> es es un texto de prueba </h1>';
+});
+
+//ejemplo 2
+Route::get('/arreglo', function () {
+    $arreglo = [
+        'Id' => '1',
+        'descripcion' => 'prudunto1'
+    ];
+    return $arreglo;
+});
+
+//ejmplo 3
+Route::get('/nombre/{nombre}', function ($nombre) {
+    return '<h1> el nombre es: ' . $nombre . '</h1>' ;
+
+});
+
+//ejmplo 4
+Route::get('/cliente/{cliente?}', function ($cliente='Cliente Geneal' ) {
+    return '<h1> el cliente es: ' . $cliente . '</h1>' ;
+
+});
+
+Route::get('/ruta1', function () {
+    return '<h1> esta la vista de la ruta 1 </h1>';
+})->name('rutaNro1');
+
+Route::get('/ruta2', function () {
+    // return '<h1> esta la vista de la ruta 2 </h1>';
+    return redirect()->route('rutaNro1');
+});
+
+
+
+//ejemplo numero 6
+Route::get('/usuario/{usuario}', function ($usuario) {
+    return 'El usuario es: ' .$usuario;
+})->where('usuario', '[0-9]+');
+
+
+
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
